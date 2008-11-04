7ed3c15198dcdddb8e7688c63f805ee5f3e8341f
Angel Ortega <angel@triptico.com>
2008-11-04 10:52:25 +0100

    Avoid warnings in the 'seq' template function.

--- Artemus.pm
+++ Artemus.pm
@@ -520,7 +520,7 @@ sub new
 
 	$a->{funcs}->{env} = sub { scalar(@_) ? ($ENV{$_[0]} || '') : join(':', keys(%ENV)); };
 	$a->{funcs}->{size} = sub { scalar(@_) ? split(/\s*:\s*/, $_[0]) : 0; };
-	$a->{funcs}->{seq} = sub { join(':', $_[0] .. $_[1]); };
+	$a->{funcs}->{seq} = sub { join(':', ($_[0] || 0) .. ($_[1] || 0)); };
 
 	$a->{_abort} = 0;
 	$a->{_unresolved} = [];
