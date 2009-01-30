ba7ea9267232fffb83e394d53b3fa07baea8b589
Angel Ortega <angel@triptico.com>
2009-01-30 22:38:02 +0100

    The Artemus.pm is really a wrapper and no longer a frontend to both versions.

--- Artemus.pm
+++ Artemus.pm
@@ -4,26 +4,7 @@ use locale;
 use strict;
 use warnings;
 
-use Artemus4;
-
-$Artemus::VERSION = $Artemus4::VERSION;
-
-sub new {
-	my $class	= shift;
-	my %args	= @_;
-
-	my $self;
-
-	if (!$args{version}) {
-		$args{version} = 4;
-	}
-
-	if ($args{version} == 4) {
-		$self = Artemus4->new(%args);
-	}
-
-	return $self;
-}
+use base 'Artemus4';
 
 1;
 __END__
@@ -35,19 +16,15 @@ Artemus - Template Toolkit
 
 =head1 DESCRIPTION
 
-This is just a frontend for the two concurrent Artemus versions. The
-C<version> argument to C<new()> makes possible to select from version
-4 or 5. The rest of arguments are version-dependent.
-
-Please pick further documentation from inside the following modules:
+The following documentation is available:
 
 =head2 Artemus4
 
-L<Artemus4>, the stable 4.x version.
+Module API and general description in L<Artemus4>.
 
-=head2 Artemus5
+=head2 artemus_cmd
 
-L<Artemus5>, the faster, compiled 5.x version.
+Usage for the command line utility in L<artemus_cmd>.
 
 =head1 AUTHOR
 
