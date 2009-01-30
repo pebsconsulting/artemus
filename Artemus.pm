04a071ef3148cc071bd3b265d3a7f851e2606981
Angel Ortega <angel@triptico.com>
2009-01-30 13:40:07 +0100

    Added documentation to Artemus.pm.

--- Artemus.pm
+++ Artemus.pm
@@ -27,3 +27,28 @@ sub new {
 
 1;
 __END__
+=pod
+
+=head1 NAME
+
+Artemus - Template Toolkit
+
+=head1 DESCRIPTION
+
+This is just a frontend for the two concurrent Artemus versions. The
+C<version> argument to C<new()> makes possible to select from version
+4 or 5. The rest of arguments are version-dependent.
+
+Please pick further documentation from inside the following modules:
+
+=head2 Artemus4
+
+L<Artemus4>, the stable 4.x version.
+
+=head2 Artemus5
+
+L<Artemus5>, the faster, compiled 5.x version.
+
+=head1 AUTHOR
+
+Angel Ortega angel@triptico.com
