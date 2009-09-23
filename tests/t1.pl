9c1dc7f654ff53cc73f450544e32cf52240b19d6
Angel Ortega <angel@triptico.com>
2009-09-23 12:04:00 +0200

    Moved tests.pl to tests/ and deleted old cruft there.

--- tests/t1.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-
-use Artemus5;
-
-my $artemus = Artemus5->new( path => [ '.' ]);
-
-my $stream = $artemus->_compile(join('', <>));
-print $artemus->execute($stream, '1st arg', '2nd arg');
