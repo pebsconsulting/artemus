6952d31636cf3bf2c11c9b62ea9554808cadf3c0
Angel Ortega <angel@triptico.com>
2009-09-30 11:15:39 +0200

    Modified tests.pl to be really a testing unit.

--- tests/tests.pl
+++ tests/tests.pl
@@ -2,86 +2,39 @@
 
 use Art5;
 
-my $a = Art5->new();
+$art5 = Art5->new();
 
-#my $src = join('', <>);
+$tests = 0;
+$tests_ok = 0;
 
-#print $a->process($src);
+sub try {
+	my $code	= shift;
+	my $expected	= shift;
 
-# <a href = http://host?t=<{$0}>;offset=0><{or $1 "Main index"}></a>
+	my $result = $art5->process($code);
 
-my $url = [ '?',
-		[ '"', '<a href = http://host/?t=' ],
-		[ '$', 0 ],
-		[ '"', ';offset='],
-		[ 'or',
-			[ '$', 2 ],
-			[ '"', 0 ],
-		],
-		[ '"', '>'],
-		[ 'or',
-			[ '$', 1 ],
-			[ '"', 'Main index' ],
-		],
-		[ '"', '</a>' ]
-	];
+	if ($result eq $expected) {
+		print "OK test: ", $code, "\n";
+		$tests_ok++;
+	}
+	else {
+		print "ERROR test: ", $code, "\n";
+		print "\tExpected: '", $expected, "' ",
+			"Got: '", $result, "'\n";
+	}
 
-$a->{op}->{url} = $url;
+	$tests++;
+}
 
-# navigator
-# <span class = 'prev'><{
-#	if $2
-#	{?
-#		"<a href = "
-#		{url "INDEX" "topic" $0 "offset" {sub $2 $1}}
-#		">&lt;&lt;</a>"
-#	}
-#}></span>
+try('1<{}>2', '12');
+try('1<{#}>2', '12');
+try('1<{#this is a comment}>2', '12');
+try('1<{#this is a comment "hello"}>2', '12');
+try('1<{"hello"}>2', '1hello2');
+try("1<{'hello'}>2", '1hello2');
+try('1<{"hello\n"}>2', "1hello\n2");
 
-# <html>\n<{url 'LOGIN' 'Login page'}>\n<{url 'INDEX'}></html>
-
-my $p = [ '?',
-		[ '"', "<html>\n" ],
-		[ 'url', [ '"', 'LOGIN'], ['"', 'Login page'], ['"', 10 ]],
-		[ '"', "\n" ],
-		[ 'url', [ '"', 'INDEX'] ],
-		[ '"', "\n</html>" ]
-	];
-
-print $a->exec($p), "\n";
-
-my $c;
-$c = $a->compile("Leading <{%arch}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{? 'Arch: ' %arch}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{mul {add 10 20} 1000}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{? {= 'TEST' 'here'} { TEST }}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{if {eq 1 2} 'Equal' 'Different'}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{if {eq 1 1} 'Equal' 'Different'}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{if {eq 1 2} {'Equal'} {'Different'}}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{if {eq 1 1} {'Equal'} {'Different'}}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{url 'LOGIN' 'Login page'}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{random 1 2 3 4 5 6}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{env}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{foreach env}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{foreach env \$0}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{foreach env \$0 ', '}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{foreach env {? \$0 '=' {env \$0}} ', '}> Trailing");
-print $a->exec($c), "\n";
-$c = $a->compile("Leading <{1 2 3}> Trailing");
-print $a->exec($c), "\n";
+print "\nTest result: ", $tests_ok, '/', $tests, ' (', ($tests_ok / $tests) * 100, "%)\n";
 
 exit 0;
+
