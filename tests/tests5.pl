8ba9a9232140503f04053f8c03b80b7ab754920a
Angel Ortega <angel@triptico.com>
2009-12-14 16:18:29 +0100

    Added test for arrays.

--- tests/tests5.pl
+++ tests/tests5.pl
@@ -77,6 +77,7 @@ try('1<{foreach {sort ary3 $1}}>2', '1132792');
 try('1<{foreach {sort ary3 {add 100 $0}}}>2', '1123792');
 try('1<{case %arch "Windows" "Is Windows" "Unix" "Is Unix"}>2', '1Is Unix2');
 try('1<{case %arch "Windows" "Is Windows" "MSDOS" "Is MSDOS" "Is Unix"}>2', '1Is Unix2');
+try("1<{foreach {& 1 2 3 4}}>2", "112342");
 
 print "\nTesting success: ", $tests_ok, '/', $tests, ' (', ($tests_ok / $tests) * 100, "%)\n";
 
