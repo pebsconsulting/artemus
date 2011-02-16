0fed3ebbc219ba2d92b7ce6dcac8667d348a8dd5
Angel Ortega <angel@triptico.com>
2011-02-16 17:25:48 +0100

    Version 5.0.1 RELEASED.

--- Art5.pm
+++ Art5.pm
@@ -2,7 +2,7 @@
 #
 #   Artemus - Template Toolkit version 5
 #
-#   Copyright (C) 2000/2010 Angel Ortega <angel@triptico.com>
+#   Copyright (C) 2000/2011 Angel Ortega <angel@triptico.com>
 #
 #   This program is free software; you can redistribute it and/or
 #   modify it under the terms of the GNU General Public License
@@ -30,7 +30,7 @@ use strict;
 use warnings;
 use Carp;
 
-$Art5::VERSION = '5.0.1-dev';
+$Art5::VERSION = '5.0.1';
 
 sub parse {
 	my $self	= shift;
